1a19208d70a40a8fa23ee560c1575250db16ad32
Satyam Tripathi <130034995+satymtripathi@users.noreply.github.com>
2025-12-13 13:54:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-# Convert static asset files
 python manage.py collectstatic --noinput
-
-# Apply any outstanding database migrations
 python manage.py migrate
+
+python manage.py createsuperuser --noinput || true
